02cf1408701954c3ef2bd128f9100425b6487b81
Phillip Truong <philliptruong1@hotmail.com>
2018-05-27 15:20:07 +1000

    first commit

---
+++ Random Decider/MBFPuppy.h
@@ -0,0 +1,13 @@
+//
+//  MBFPuppy.h
+//  Random Decider
+//
+//  Created by phillip truong on 15/01/2015.
+//  Copyright (c) 2015 sbornes-portfolio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFPuppy : MBFDog
+
+@end
---
+++ Random Decider/MBFPuppy.m
@@ -0,0 +1,13 @@
+//
+//  MBFPuppy.m
+//  Random Decider
+//
+//  Created by phillip truong on 15/01/2015.
+//  Copyright (c) 2015 sbornes-portfolio. All rights reserved.
+//
+
+#import "MBFPuppy.h"
+
+@implementation MBFPuppy
+
+@end
